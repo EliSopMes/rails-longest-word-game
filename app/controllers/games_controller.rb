a2928c27d18345085c1117bb369e6bb60baef8ee
Elisabeth Messmer <elisabethsophiem@web.de>
2023-02-25 15:07:03 +0100

    styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,41 +1,42 @@
-require "open-uri"
-require "json"
+require 'open-uri'
+require 'json'
 
+# This ia the GamesController documentation comment
 class GamesController < ApplicationController
-  before_action :set_games, only: [:new, :score]
-
   def new
-    @letters = (0...10).map { (65 + rand(26)).chr }
+    # @letters = (0...10).map { (65 + rand(26)).chr }
+    @letters = []
+    let = ('A'..'Z').to_a
+    vowel = %w[A E I O U]
+    2.times do
+      @letters << vowel[rand(0..4)]
+    end
+    8.times do
+      @letters << let[rand(0..25)]
+    end
+    @letters
   end
 
   def score
-    @valid_inputs = []
-    @score = 0
     @answer = params[:answer].upcase.chars
+    @letters = params[:letters].split
     if grid_check?
-      if english?
-        @result = 'valid'
-        @valid_inputs << params[:answer]
-        @score += params[:answer].length
-        # @valid_inputs.each do |word|
-        #   @score += word.length
-        # end
-      else
-        @result = 'invalid'
-      end
+      english? ? @result = 'valid' : @result = 'invalid'
     else
       @result = 'not in grid'
     end
   end
 
+  private
+
   def grid_check?
     answer = params[:answer].upcase.chars
-    answer.all? { |letter| params[:array].include?(letter) }
+    answer.all? { |letter| params[:letters].include?(letter) }
   end
 
   def english?
     response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:answer]}")
     json = JSON.parse(response.read)
-    json["found"]
+    json['found']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/new/:score', to: 'games#new'
+  # get '/new/:score', to: 'games#new'
+  # post 'score', to: 'games#score'
+  root to: 'games#new'
   post 'score', to: 'games#score'
 end
